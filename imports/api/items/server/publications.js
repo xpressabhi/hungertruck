482defa9e9503f65470e68500092ddb1f77bbcaa
abhishek <akmnitt@gmail.com>
2018-05-10 11:08:18 +0530

    UI updates

--- imports/api/items/server/publications.js
+++ imports/api/items/server/publications.js
@@ -6,3 +6,8 @@ import { Items } from '../items.js';
 Meteor.publish("items.user", function(){
   return Items.find({userId:this.userId});
 });
+
+Meteor.publish("items.all", function(userIds){
+  check(userIds, [String]);
+  return Items.find({userId:{$in:userIds}});
+});
--- imports/ui/components/alltrucks/alltrucks.js
+++ imports/ui/components/alltrucks/alltrucks.js
@@ -1,25 +1,27 @@
 import './alltrucks.html';
 import {Locations} from '/imports/api/locations/locations.js';
 import {Trucks} from '/imports/api/trucks/trucks.js';
+import {Items} from '/imports/api/items/items.js';
 
 var markers = [];
 
-Template.alltrucks.onCreated(function(){
+Template.alltrucks.onCreated(function() {
   this.autorun(() => {
     this.subscribe('locations.online');
     let userIds = [];
     Locations.find().map((l) => {
       userIds.push(l.userId);
     });
-    this.subscribe('trucks.all',userIds);
+    this.subscribe('trucks.all', userIds);
+    this.subscribe('items.all', userIds);
   });
   this.latlng = new ReactiveVar();
-  var self=this;
-  GoogleMaps.ready('map', function (map) {
+  var self = this;
+  GoogleMaps.ready('map', function(map) {
     console.log(map);
     let usermarker;
     const image = {
-      url: 'blue-dot.png',
+      url: 'blue-dot.png'
     };
     var truckImage = {
       url: 'food-truck.png',
@@ -37,110 +39,105 @@ Template.alltrucks.onCreated(function(){
       scaledSize: new google.maps.Size(35, 35),
       setMyLocationEnabled: true
     };
-  let infowindow;
+    let infowindow;
     // Create and move the marker when latLng changes.
-    self.autorun(function () {
+    self.autorun(function() {
       if (markers) {
         for (i in markers) {
           markers[i].setMap(null);
         }
       }
-      Locations.find({
-          state: true
-        }).forEach((p) => {
-          var marker = new google.maps.Marker({
-            title: "my truck",
+      Locations.find({state: true}).forEach((p) => {
+        var marker = new google.maps.Marker({
+          title: "my truck",
           //  animation: google.maps.Animation.DROP,
-            draggable: p.userId === Meteor.userId(),
-            icon: p.userId === Meteor.userId() ? myTruck: truckImage,
-            position: new google.maps.LatLng(p.lat, p.lng),
-            map: map.instance,
-          });
+          draggable: p.userId === Meteor.userId(),
+          icon: p.userId === Meteor.userId()
+            ? myTruck
+            : truckImage,
+          position: new google.maps.LatLng(p.lat, p.lng),
+          map: map.instance
+        });
 
-          marker.addListener('click', function() {
-            console.log('clicking icon');
-            console.log(Trucks.find().count());
-            var truck = Trucks.findOne({
-              userId: p.userId
+        marker.addListener('click', function() {
+          console.log('clicking icon');
+          console.log(Trucks.find().count());
+          var truck = Trucks.findOne({userId: p.userId});
+          console.log(truck);
+          console.log('clicking icon');
+          if (truck) {
+            var contentString = `<div>  <b> <i class="fas fa-truck fa-fw"></i>:${truck.name}</b> <br/> <i class="fas fa-mobile fa-fw"></i> : ${truck.mobile} <br/>`;
+            Items.find({userId:p.userId}).map((i) => {
+              contentString+=`<i class="fas fa-utensils"></i> : ${i.name},${i.rate} <br/>`;
             });
-            console.log(truck);
-            console.log('clicking icon');
-            if (truck) {
-              var contentString = `<div>  <b> This is my truck </b> ${truck.name} ${p.userId}</div>`;
-              if (infowindow) {
-                infowindow.close();
-              }
-              infowindow = new google.maps.InfoWindow({
-                content: contentString
-              });
-              infowindow.open(map, marker);
+            contentString+=`</div>`;
+            if (infowindow) {
+              infowindow.close();
             }
-          });
+            infowindow = new google.maps.InfoWindow({content: contentString});
+            infowindow.open(map, marker);
+          }
+        });
 
-          marker.addListener('dragend', function(event) {
+        marker.addListener('dragend', function(event) {
           //  Meteor.call('deleteLocation', Meteor.userId());
           Meteor.call('locations.update', event.latLng.lat(), event.latLng.lng());
           console.log('dragging');
-          });
-          markers.push(marker);
         });
+        markers.push(marker);
+      });
 
-        const latLng = Geolocation.latLng();
-        console.log(latLng);
-        if (!latLng)
-          return;
+      const latLng = Geolocation.latLng();
+      console.log(latLng);
+      if (!latLng)
+        return;
 
-        // If the marker doesn't yet exist, create it.
-        if (!usermarker) {
-          usermarker = new google.maps.Marker({
-            position: new google.maps.LatLng(latLng.lat, latLng.lng),
-            map: map.instance,
-            icon: image
-          });
-        //  markersArray.push(marker); // The marker already exists, so we'll just change its position.);
-        } else {
-          usermarker.setPosition(latLng);
-        }
+      // If the marker doesn't yet exist, create it.
+      if (!usermarker) {
+        usermarker = new google.maps.Marker({
+          position: new google.maps.LatLng(latLng.lat, latLng.lng),
+          map: map.instance,
+          icon: image
+        });
+        //  markersArray.push(marker);  The marker already exists, so we'll just change its position.);
+      } else {
+        usermarker.setPosition(latLng);
+      }
 
-        // Center and zoom the map view onto the current position.
-        map.instance.setCenter(usermarker.getPosition());
-        map.instance.setZoom(14);
-        map.instance.setClickableIcons(false);
+      // Center and zoom the map view onto the current position.
+      map.instance.setCenter(usermarker.getPosition());
+      map.instance.setZoom(14);
+      map.instance.setOptions({mapTypeControl: false, streetViewControl: false, clickableIcons: false, fullscreenControl: false});
     });
   });
 });
 
-Template.alltrucks.onRendered(function(){
-});
+Template.alltrucks.onRendered(function() {});
 
 Template.alltrucks.helpers({
-  showMap(){
-    const userId= Meteor.userId();
+  isOnline() {
+    const userId = Meteor.userId();
     if (userId) {
       if (Roles.userIsInRole(userId, 'truck')) {
-        const online = Locations.findOne({userId:userId, state:true});
+        const online = Locations.findOne({userId: userId, state: true});
         if (online) {
           return true;
-        }else {
-          let latlng = Template.instance().latlng.get();
-          if (!latlng) {
-            latlng = Geolocation.latLng();
-            Template.instance().latlng.set(latlng);
-          }
-          return false;
         }
-      }else {
-      return true;
       }
-    }else {
-    return true;
     }
-
+    return false;
   },
   mapOptions: function() {
+    let latlng = Template.instance().latlng.get();
+    if (!latlng) {
+      latlng = Geolocation.latLng();
+      Template.instance().latlng.set(latlng);
+    }
     if (GoogleMaps.loaded()) {
       return {
-        center: new google.maps.LatLng(17.4591369,78.3442305),
+        center: latlng
+          ? new google.maps.LatLng(latlng.lat, latlng.lng)
+          : new google.maps.LatLng(17.4635, 78.3473),
         zoom: 14
       };
     } else {
@@ -160,9 +157,7 @@ Template.alltrucks.events({
       console.log('no location found');
     }
   },
-  'click .goOffline'(event, templateInstance) {
-    Meteor.call('locations.allOffline',() => {
-
-    })
+  'click .goOffline' (event, templateInstance) {
+    Meteor.call('locations.allOffline', () => {})
   }
 });
