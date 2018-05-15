d1438d9eb829593ab329afa38143e0f85472b2b3
abhishek <akmnitt@gmail.com>
2018-05-15 10:22:09 +0530

    crop only truck image

--- imports/api/images/images.js
+++ imports/api/images/images.js
@@ -1,5 +1,11 @@
 var createThumb = function(fileObj, readStream, writeStream) {
-  gm(readStream).autoOrient().resize(400).gravity('Center').crop(400,300,0,0).stream().pipe(writeStream);
+  //console.log(fileObj);
+  if (fileObj.imageOf === 'Truck') {
+    gm(readStream).autoOrient().resize(400).gravity('Center').crop(400,300,0,0).stream().pipe(writeStream);
+  }else {
+    gm(readStream).autoOrient().resize(400).stream().pipe(writeStream);
+  }
+
   //gm(readStream, fileObj.name()).resize(400).crop(400,300,0,0).stream().pipe(writeStream);
 };
 
