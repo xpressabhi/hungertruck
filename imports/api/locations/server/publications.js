52deb08d52c38f4a5409163ce2f7bd24d3e36cdd
abhishek <akmnitt@gmail.com>
2018-05-23 10:03:38 +0530

    display only verified trucks

--- imports/api/locations/server/publications.js
+++ imports/api/locations/server/publications.js
@@ -1,12 +1,18 @@
 // All links-related publications
 
-import { Meteor } from 'meteor/meteor';
-import { Locations } from '../locations.js';
+import {Meteor} from 'meteor/meteor';
+import {Locations} from '../locations.js';
 
-Meteor.publish("locations.user", function(){
-  return Locations.find({userId:this.userId});
+Meteor.publish("locations.user", function() {
+  return Locations.find({userId: this.userId});
 });
 
-Meteor.publish("locations.online", function(){
-  return Locations.find({lastLocation:true});
+Meteor.publish("locations.online", function() {
+  let userIds = Meteor.users.find({roles: 'verified-truck'}).map(u => u._id);
+  return Locations.find({
+    userId: {
+      $in: userIds
+    },
+    lastLocation: true
+  });
 });
