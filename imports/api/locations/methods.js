86dfa99f30465bfe89330499dd94bdb57764b7f7
abhishek <akmnitt@gmail.com>
2018-05-26 11:41:53 +0530

    auto offline

--- imports/api/locations/methods.js
+++ imports/api/locations/methods.js
@@ -3,6 +3,7 @@
 import {Meteor} from 'meteor/meteor';
 import {check} from 'meteor/check';
 import {Locations} from './locations.js';
+import {Schedules} from '/imports/api/schedules/schedules.js';
 
 Meteor.methods({
   "locations.insert" (lat, lng, state) {
@@ -10,9 +11,23 @@ Meteor.methods({
     check(lng, Number);
     check(state, Boolean);
     if (this.userId) {
-        Locations.update({userId:this.userId,state:true},{$set:{state:false}},{multi:true});
-        Locations.update({userId:this.userId,lastLocation:true},{$set:{lastLocation:false}},{multi:true});
-        return Locations.insert({lat,lng,state});
+      Locations.update({
+        userId: this.userId,
+        state: true
+      }, {
+        $set: {
+          state: false
+        }
+      }, {multi: true});
+      Locations.update({
+        userId: this.userId,
+        lastLocation: true
+      }, {
+        $set: {
+          lastLocation: false
+        }
+      }, {multi: true});
+      return Locations.insert({lat, lng, state});
     }
     return;
   },
@@ -23,25 +38,83 @@ Meteor.methods({
     }
     return;
   },
-  "locations.allOffline"(userId){
-    check(userId, Match.OneOf(String,null,undefined));
+  "locations.allOffline" (userId) {
+    check(userId, Match.OneOf(String, null, undefined));
     userId = userId || this.userId;
-    return Locations.update({userId:userId,state:true},{$set:{state:false}},{multi:true});
+    return Locations.update({
+      userId: userId,
+      state: true
+    }, {
+      $set: {
+        state: false
+      }
+    }, {multi: true});
   },
-  "locations.update"(lat, lng){
+  "locations.update" (lat, lng) {
     check(lat, Number);
     check(lng, Number);
     if (this.userId) {
-      return Locations.update({userId:this.userId,state:true},{$set:{lat,lng}});
+      return Locations.update({
+        userId: this.userId,
+        state: true
+      }, {
+        $set: {
+          lat,
+          lng
+        }
+      });
     }
   },
-  "locations.updatelastLocation"(){
+  "locations.updatelastLocation" () {
     Meteor.users.find().map((u) => {
-      console.log('processing for ',u._id);
-      Locations.update({userId:u._id},{$set:{lastLocation:false}},{multi:true});
-      Locations.find({userId:u._id},{sort:{createdAt:-1},limit:1}).map((l) => {
-        Locations.update({_id:l._id},{$set:{lastLocation:true}});
+      console.log('processing for ', u._id);
+      Locations.update({
+        userId: u._id
+      }, {
+        $set: {
+          lastLocation: false
+        }
+      }, {multi: true});
+      Locations.find({
+        userId: u._id
+      }, {
+        sort: {
+          createdAt: -1
+        },
+        limit: 1
+      }).map((l) => {
+        Locations.update({
+          _id: l._id
+        }, {
+          $set: {
+            lastLocation: true
+          }
+        });
       });
+    });
+  },
+  "locations.setOfflineAuto" () {
+    Locations.find({state: true}).map((l) => {
+      Schedules.find({userId: l.userId}).map((sch) => {
+        const dt = new Date();
+        let dtEnd = new Date();
+        let [endHours, endMins, endApm] = sch.end.split(' ');
+        endHours = endApm === 'PM'
+          ? Number(endHours) + 12
+          : endHours
+        dtEnd.setHours(endHours, endMins, 0);
+        if ((dt - dtEnd) > 0) 
+          Locations.update({
+            userId: sch.userId,
+            state: true
+          }, {
+            $set: {
+              state: false
+            }
+          }, {multi: true});
+
+        }
+      )
     });
   }
 });
